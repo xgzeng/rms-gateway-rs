f6e99033ce47da2d31c31f53d9236c6ba71c6b29
XG Zeng <xgzeng@gmail.com>
2026-01-30 11:01:42 +0800

    apply compatibility transform to V1 route only

--- src/main.rs
+++ src/main.rs
@@ -60,24 +60,26 @@ async fn main() -> anyhow::Result<()> {
     let tinc_svc_rms = RmsConfigApiTinc::new(RmsApiGateway::new(rms_endpoint).await);
     let tinc_svc_gsc = GscGatewayApiTinc::new(GscApiProxy::new(gsc_endpoint).await);
 
+    // V2 API
     let api_router = tinc_svc_rms.into_router().merge(tinc_svc_gsc.into_router());
+    let app = app.nest("/rms/api/v2", api_router.clone());
 
+    // V1 API with backward compatibility layers
     // Transform json body keys between lowerCamelCase and snake_case for backward compatibility
-    let api_router = api_router.layer(tower_http::map_request_body::MapRequestBodyLayer::new(
+    let api_v1_router = api_router.layer(tower_http::map_request_body::MapRequestBodyLayer::new(
         |body| map_json_body(body, json_convert::json_keys_to_snake_case),
     ));
-    let api_router = api_router.layer(tower_http::map_response_body::MapResponseBodyLayer::new(
-        |body| map_json_body(body, json_convert::json_keys_to_lower_camel_case),
-    ));
-
-    let app = app.nest("/rms/api/v1", api_router.clone());
-    let app = app.nest("/rms/api/v2", api_router);
-
-    // add content-type for backward compatibility
-    let app = app.layer(SetRequestHeaderLayer::overriding(
+    let api_v1_router =
+        api_v1_router.layer(tower_http::map_response_body::MapResponseBodyLayer::new(
+            |body| map_json_body(body, json_convert::json_keys_to_lower_camel_case),
+        ));
+    // Add content-type for backward compatibility
+    let api_v1_router = api_v1_router.layer(SetRequestHeaderLayer::overriding(
         CONTENT_TYPE,
         HeaderValue::from_static("application/json"),
     ));
+
+    let app = app.nest("/rms/api/v1", api_v1_router.clone());
 
     // run it
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
